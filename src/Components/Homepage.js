1bba66e6489a2eff91fe5261816b2e1519e334b3
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2020-07-16 18:26:10 -0400

    Added proper text and a logo to Homepage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import '../Styles/Home.css';
+import '../Styles/Homepage.css';
+import Logo from '../Styles/Images/university_logo.jpg';
 
 //Initial landing page
 class Homepage extends Component{
     render(){
         return(
             <div className="Homepage">
-                aaaaa
+                Welcome to our Student and Campus Manager
+                <img src = {Logo} alt ="Logo" class = "center"
+                height = {700}
+                width = {700}
+                />
+
             </div>
-            
         );
     }
 }
 
 
 export default Homepage;
+
