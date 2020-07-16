370cb2ffb9cb607a6d9c7b2d2d0b48cd6082216e
Jsze5341 <33404576+Jsze5341@users.noreply.github.com>
2020-07-16 17:34:20 -0400

    Added Homepage Component

---
+++ src/Components/Homepage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import '../Styles/Home.css';
+
+//Initial landing page
+class Homepage extends Component{
+    render(){
+        return(
+            <div className="Homepage">
+                aaaaa
+            </div>
+            
+        );
+    }
+}
+
+
+export default Homepage;
